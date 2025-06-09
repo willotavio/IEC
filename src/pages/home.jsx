ff64092fcc5ccd0064aaa06f9e7545adea3e70fb
willotavio <89710130+willotavio@users.noreply.github.com>
2025-06-09 16:13:45 -0300

    Adicionando parâmetro para nome na página home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom"
+
 export default function Home() {
+    const params = useParams()
+
     return(
         <div className='home'>
-            <p>Bem-vindo(a)!</p>
+            <p>Bem-vindo(a), { params.nome ?? "anônimo" }!</p>
         </div>
     )
 }
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -5,7 +5,7 @@ export default function AppRoutes() {
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/:nome?" element={<Home />} />
             </Routes>
         </BrowserRouter>
     )
