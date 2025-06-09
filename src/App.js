3003f131705f8e1abf3bbe3ef9a8250f4226b599
willotavio <89710130+willotavio@users.noreply.github.com>
2025-06-09 16:08:14 -0300

    Adicionando rotas e página home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import AppRoutes from './routes/AppRoutes.js';
 
 function App() {
   return (
-    <div className='home'>
-      <p>Bem-vindo(a)!</p>
-    </div>
+    <AppRoutes />
   );
 }
 
---
+++ src/pages/home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return(
+        <div className='home'>
+            <p>Bem-vindo(a)!</p>
+        </div>
+    )
+}
